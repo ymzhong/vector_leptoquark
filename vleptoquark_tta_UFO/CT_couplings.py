d148ffdb07d2c18e60099f8fb9f6313ca21d29ce
Yi-Ming Zhong <zym@Yimings-iMac.local>
2020-09-21 16:20:55 -0500

    third commit

---
+++ vleptoquark_tta_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.36
+# Mathematica version: 12.0.0 for Linux x86 (64-bit) (April 7, 2019)
+# Date: Sun 6 Sep 2020 13:35:24
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ vleptoquark_tta_UFO/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.36
+# Mathematica version: 12.0.0 for Linux x86 (64-bit) (April 7, 2019)
+# Date: Sun 6 Sep 2020 13:35:23
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+LambdaOrder = CouplingOrder(name = 'LambdaOrder',
+                            expansion_order = 99,
+                            hierarchy = 1)
+
