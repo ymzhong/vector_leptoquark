36374849e81ab9ee6a317e98e91fb52a73fcda11
Yi-Ming Zhong <myhoneyclover@hotmail.com>
2022-08-11 10:30:44 -0500

    scalar leptoquark model couples to UE
    A scalar leptoquark model contains 9 independent couplings to u e/ u mu/ u ta/ c e/ c mu/ c ta/ t e/ t mu/ t ta.

---
+++ sleptoquark_UE_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.36
+# Mathematica version: 12.0.0 for Linux x86 (64-bit) (April 7, 2019)
+# Date: Tue 18 Jan 2022 23:49:26
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
