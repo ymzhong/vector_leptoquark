c90c2a6e57b9ac190a6a55d8e2bf91bf079a6cad
Yi-Ming Zhong <myhoneyclover@hotmail.com>
2022-08-11 10:35:50 -0500

    vector leptoquark model couples to QL triplet

---
+++ vleptoquark_QL_triplet_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 12.3.0 for Linux x86 (64-bit) (May 10, 2021)
+# Date: Fri 1 Jul 2022 09:11:11
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
